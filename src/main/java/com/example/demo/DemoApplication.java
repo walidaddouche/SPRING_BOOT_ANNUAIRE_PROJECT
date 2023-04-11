00f4380b1fd09c2c9c9aacaa01bb80e3c02a9401
zidanifahed <fahed-imed.zidani@etu.yniv-amu.fr>
2023-04-11 17:06:07 +0200

    generator

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -28,6 +28,8 @@ public class DemoApplication {
             personRepository.save(person);
         }
 
+
+
     }
 
 }
---
+++ src/main/java/com/example/demo/model/GenearatorUtility.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import com.example.demo.repository.GroupTableRepository;
+import com.example.demo.repository.PersonRepository;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class GenearatorUtility {
+
+    private final Generator generator;
+    private final GroupTableRepository groupRepository;
+    private final PersonRepository personRepository;
+
+
+    public void generateData() {
+        List<GroupTable> groups = generator.generateGroups(400);
+        groupRepository.saveAll(groups);
+
+        List<Person> people = generator.generatePersons(1000, groups);
+        personRepository.saveAll(people);
+    }
+}

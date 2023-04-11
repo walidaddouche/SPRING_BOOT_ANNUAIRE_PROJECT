9c18ffa7e200119f0564d973ffc9f2c4adcb6573
ADDOUCHE Walid <walid.addouche@etu.univ-amu.fr>
2023-04-11 17:31:34 +0200

    Groupe controller dosent work

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+
+import com.example.demo.model.GroupTable;
 import com.example.demo.model.Person;
+import com.example.demo.repository.GroupTableRepository;
 import com.example.demo.repository.PersonRepository;
 import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
@@ -8,15 +11,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) throws ParseException {
-		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-	    PersonRepository personRepository = context.getBean(PersonRepository.class);
+
+
+    public static void main(String[] args) throws ParseException {
+        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+        PersonRepository personRepository = context.getBean(PersonRepository.class);
+        GroupTableRepository groupRepository = context.getBean(GroupTableRepository.class);
+
         Faker faker = new Faker();
+        List<GroupTable> groups = new ArrayList<>();
+
+        for (int i = 0; i < 200; i++) {
+            GroupTable group = new GroupTable();
+            group.setName(faker.company().name());
+            groupRepository.save(group);
+            groups.add(group);
+
+        }
+
+
+
         for (int i = 0; i < 1000; i++) {
             Person person = new Person();
             person.setFirstName(faker.name().firstName());
@@ -25,8 +46,15 @@ public class DemoApplication {
             person.setWebsite(faker.internet().url());
             person.setDateOfBirth(faker.date().birthday());
             person.setPassword(faker.internet().password());
+            person.setGroup(groups.get(faker.random().nextInt(groups.size())));
             personRepository.save(person);
         }
+
+
+
+
+
+
 
 
 
--- src/main/java/com/example/demo/controller/GroupController.java
+++ src/main/java/com/example/demo/controller/GroupController.java
@@ -3,10 +3,11 @@ package com.example.demo.controller;
 import com.example.demo.model.GroupTable;
 import com.example.demo.repository.GroupTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/groups")
 
 public class GroupController {
@@ -14,20 +15,12 @@ public class GroupController {
     private GroupTableRepository groupRepository;
 
 
-    @GetMapping("/groups")
+    @GetMapping("/all")
     public String showAllGroups(Model model) {
-        model.addAttribute("groups", groupRepository.findAll());
-        return "group";
-    }
-
-    @GetMapping("/groups/{id}")
-    public String showGroupDetails(@PathVariable Long id, Model model) {
-        GroupTable group = groupRepository.findById(id).orElse(null);
-        if (group == null) {
-            return "redirect:/groups";
-        }
-        model.addAttribute("group", group);
-        return "group-details";
+        //model.addAttribute("groups", groupRepository.findAll());
+        System.out.println(groupRepository.findAll());
+        System.out.println("HERE");
+        return "groups";
     }
 }
 
